eee079c374f1efa71c9967671216d448cc5f7e30
Mason <masoncfrancis@gmail.com>
2024-11-10 17:33:03 -0500

    Changed to navbar from tabs

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,40 +1,28 @@
 // pages/index.tsx
 import { useState } from 'react';
-import { Container, Tab, Tabs } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const HomePage: React.FC = () => {
   const [key, setKey] = useState<string>('main');
 
   return (
-    <Container className="my-5">
-      <h1 className="text-center mb-4">Home Page</h1>
-      
-      <Tabs
-        id="controlled-tab-example"
-        activeKey={key}
-        onSelect={(k) => setKey(k || 'main')}
-        className="mb-3"
-      >
-        <Tab eventKey="main" title="Main">
-          <p>Welcome to the main page content.</p>
-        </Tab>
-        <Tab eventKey="water" title="Water">
-          <p>Add information about water systems here</p>
-        </Tab>
-        <Tab eventKey="hvac" title="HVAC">
-          <p>Add information about HVAC systems here</p>
-        </Tab>
-        <Tab eventKey="electric" title="Electrical">
-          <p>Add information about electrical systems here</p>
-        </Tab>
-        <Tab eventKey="appliances" title="Appliances">
-          <p>Add informatino about your appliances here</p>
-        </Tab>
-        <Tab eventKey="cosmetics" title="Cosmetics">
-          <p>Add information about cosmetic work done on your home here.</p>
-        </Tab>
-      </Tabs>
+    <Container>
+      <Navbar expand="lg">
+        <Navbar.Brand href="#home">HomeLogger</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#">Water + Sewer</Nav.Link>
+            <Nav.Link href="#">HVAC</Nav.Link>
+            <Nav.Link href="#">Electrical</Nav.Link>
+            <Nav.Link href="#">Appliances</Nav.Link>
+            <Nav.Link href="#">Exterior</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <p id="main">Welcome to the main page content.</p>
     </Container>
+
   );
 };
 
