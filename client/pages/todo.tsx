dd56a21ef1e61bea6d89b5c4cdf1df69ceab01dd
Mason <masoncfrancis@gmail.com>
2024-11-15 12:28:42 -0700

    Added items to todo page to check styling

--- client/pages/todo.tsx
+++ client/pages/todo.tsx
@@ -2,6 +2,9 @@
 import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import MyNavbar from '../components/Navbar';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+
 
 const TodoPage: React.FC = () => {
   const [key, setKey] = useState<string>('main');
@@ -9,7 +12,13 @@ const TodoPage: React.FC = () => {
   return (
     <Container>
       <MyNavbar />
-      <p id='maintext'>Welcome to the to-do page.</p>
+      <p id='maintext'>To do:</p>
+      <ListGroup>
+        <ListGroup.Item>Item 1</ListGroup.Item>
+        <ListGroup.Item>Item 2</ListGroup.Item>
+        <ListGroup.Item>Item 3</ListGroup.Item>
+      </ListGroup>
+    
     </Container>
 
   );
