a15ae25377cb7f2ded3e25b52f9642be4a6876e4
Mason <masoncfrancis@gmail.com>
2024-11-22 12:09:45 -0700

    Added todo item component

---
+++ client/components/TodoItem.tsx
@@ -0,0 +1,17 @@
+import Form from 'react-bootstrap/Form';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+interface TodoItemProps {
+    label: string;
+    checked: boolean;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({ label, checked }) => {
+  return (
+    <ListGroup.Item>
+      <Form.Check type='checkbox' label={label} checked={checked} />
+    </ListGroup.Item>
+  );
+};
+
+export default TodoItem;
--- client/pages/todo.tsx
+++ client/pages/todo.tsx
@@ -1,33 +1,36 @@
-// pages/index.tsx
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import MyNavbar from '../components/Navbar';
 import ListGroup from 'react-bootstrap/ListGroup';
-import Form from 'react-bootstrap/Form';
+import TodoItem from '../components/TodoItem';
 
-
+const jsonObject = {
+  "todo": [
+    {
+      "label": "Change anode rod",
+      "checked": false
+    },
+    {
+      "label": "Change air filter",
+      "checked": true
+    }
+  ]
+}
 
 const TodoPage: React.FC = () => {
   const [key, setKey] = useState<string>('main');
+  const [todos, setTodos] = useState(jsonObject.todo);
 
   return (
     <Container>
       <MyNavbar />
       <p id='maintext'>To do:</p>
       <ListGroup>
-        <ListGroup.Item>
-          <Form.Check type='checkbox' label='Item 1' />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Form.Check type='checkbox' label='Item 2' />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Form.Check type='checkbox' label='Item 1' />
-        </ListGroup.Item>
+        {todos.map((todo, index) => (
+          <TodoItem key={index} label={todo.label} checked={todo.checked} />
+        ))}
       </ListGroup>
-    
     </Container>
-
   );
 };
 
