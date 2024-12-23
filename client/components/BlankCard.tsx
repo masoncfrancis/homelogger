4a7bcd8bf8286ebb63834d985a785145d8fd25b5
Mason <masoncfrancis@gmail.com>
2024-12-23 18:08:44 -0500

    Added modal for adding appliances

--- client/components/BlankCard.tsx
+++ client/components/BlankCard.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
 
 interface BlankCardProps {
-  onClick: () => void;
+    onClick: () => void;
 }
 
-const BlankCard: React.FC<BlankCardProps> = ({ onClick }) => {
-  return (
-    <div onClick={onClick} style={{ cursor: 'pointer' }}>
-      <Card style={{ width: '18rem', textAlign: 'center' }}>
-        <Card.Body>
-          <Card.Title>+</Card.Title>
-        </Card.Body>
-      </Card>
-    </div>
-  );
+const BlankCard: React.FC<BlankCardProps> = ({onClick}) => {
+    return (
+        <div onClick={onClick} style={{cursor: 'pointer'}}>
+            <Card style={{width: '18rem', textAlign: 'center'}}>
+                <Card.Body>
+                    <Card.Title>+</Card.Title>
+                </Card.Body>
+            </Card>
+        </div>
+    );
 };
 
 export default BlankCard;
--- client/pages/appliances.tsx
+++ client/pages/appliances.tsx
@@ -1,23 +1,37 @@
-import { useState } from 'react';
-import { Container } from 'react-bootstrap';
+import {useState} from 'react';
+import {Container} from 'react-bootstrap';
 import MyNavbar from '../components/Navbar';
 import BlankCard from '../components/BlankCard';
+import AddApplianceModal from '../components/AddApplianceModal';
 
 const AppliancesPage: React.FC = () => {
-  const [key, setKey] = useState<string>('main');
+    const [showModal, setShowModal] = useState(false);
 
-  const handleAddCardClick = () => {
-    // Logic to add a new card
-    console.log('Add new card clicked');
-  };
+    const handleAddCardClick = () => {
+        setShowModal(true);
+    };
 
-  return (
-    <Container>
-      <MyNavbar />
-      <p id='maintext'>Welcome to the appliances page.</p>
-      <BlankCard onClick={handleAddCardClick} />
-    </Container>
-  );
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    const handleSaveAppliance = (makeModel: string, yearPurchased: string, purchasePrice: string, location: string) => {
+        // Logic to save the new appliance
+        console.log('Appliance saved:', {makeModel, yearPurchased, purchasePrice, location});
+    };
+
+    return (
+        <Container>
+            <MyNavbar/>
+            <p id='maintext'>Welcome to the appliances page.</p>
+            <BlankCard onClick={handleAddCardClick}/>
+            <AddApplianceModal
+                show={showModal}
+                handleClose={handleCloseModal}
+                handleSave={handleSaveAppliance}
+            />
+        </Container>
+    );
 };
 
 export default AppliancesPage;
