60719efeb061c10b4e55f05d515c9fc7fa20ab7f
Mason Francis <masoncfrancis@gmail.com>
2025-03-27 17:05:34 -0400

    Removed appliances so I can work on it later, modified navbar

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -1,10 +1,8 @@
 // client/components/Navbar.tsx
-import { useRouter } from 'next/router';
 import { Nav, Navbar } from 'react-bootstrap';
 import Link from 'next/link';
 
 const MyNavbar: React.FC = () => {
-  const router = useRouter();
 
   return (
     <Navbar expand="lg">
@@ -12,9 +10,9 @@ const MyNavbar: React.FC = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link as={Link} href="/" active={router.pathname === '/'}>Home</Nav.Link>
-          <Nav.Link as={Link} href="/todo" active={router.pathname === '/todo'}>To Do</Nav.Link>
-          <Nav.Link as={Link} href="/appliances" active={router.pathname === '/appliances'}>Appliances</Nav.Link>
+          <Nav.Link as={Link} href="/">Home</Nav.Link>
+          <Nav.Link as={Link} href="/todo">To Do</Nav.Link>
+          <Nav.Link as={Link} href="/appliances">Appliances</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
