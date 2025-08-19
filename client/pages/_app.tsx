dbb0d4fe15024e3db798c6bd6486dd29fe91f0db
Mason <masoncfrancis@gmail.com>
2025-08-19 10:12:16 -0400

    Fixed issues with app not being served properly by nginx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app'
 
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+if (!process.env.NEXT_PUBLIC_SERVER_URL) {
+  throw new Error('NEXT_PUBLIC_SERVER_URL environment variable is not set, and is required.');
+}
+
+export const SERVER_URL = `${process.env.NEXT_PUBLIC_SERVER_URL}/api`;
 
 
 export default function App({ Component, pageProps }: AppProps) {
