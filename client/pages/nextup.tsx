1305f8d17b4ab0b47efde77c9549c93d1f1d14d1
Mason Francis <masoncfrancis@gmail.com>
2026-01-07 15:46:41 -0500

    Added frontend for looking at next up occurences

--- client/pages/nextup.tsx
+++ client/pages/nextup.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import MyNavbar from '../components/Navbar';
 import NextUp from '../components/NextUp';
 
-const HomePage: React.FC = () => {
+const NextUpPage: React.FC = () => {
   return (
     <Container>
       <MyNavbar />
-      <div style={{ marginTop: '1rem' }}>
+      <div style={{marginTop: '1rem'}}>
         <NextUp />
       </div>
     </Container>
   );
 };
 
-export default HomePage;
+export default NextUpPage;
